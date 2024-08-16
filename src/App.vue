ac8deb38ca2c49b23fc9971285236398ce924a8c
Vova <vovagotovko2004@gmail.com>
2024-08-16 15:00:12 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2 v-color:[type].blink="myColor">Директивы</h2>
+    <!-- [] используются дле передачи динамических параметров -->
+    <div class="form-control">
+      <label for="inp">Активный</label>
+      <input type="text" id="inp" v-focus>
+    </div>
+    <button class="btn" @click="myColor = 'darkblue'">Сделать синим!</button>
+    <button class="btn" @click="type = type == 'color' ? 'backgroundColor' : 'color'">Переключить тип</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      myColor: "darkred",
+      type: "color"
+    }
+  },
+  // Здесь мы будет создавать директиву которая будет вешать фокус на элемент, к которому она применена
+  directives: {
+    focus: {
+      mounted(el, binding){//binding – объект, в котором дохера всякой шняги
+        el.focus();
+      }
+    },
+    //более тонкая настройка кастомной директивы
+    color: {
+      //переданные параметры хранятяс в массиве binding.arg
+      //переданные модификаторы хранятся в объекте binding.modifiers где ключ это имя модификатора, а значение просто true
+      mounted(el, binding){//вызывается как известно только при создании элемента
+        console.log(el);//выведет инфу про заголовок h2
+        el.style.color = binding.value;
+        if(binding.modifiers.blink){
+          let flag = true;
+          setInterval(() => {
+            el.style.color = flag ? '#fff' : binding.value;
+            flag =! flag;
+          }, 100);
+        }
+      },
+      updated(el, binding){
+        console.log(binding);
+        el.style[binding.arg] = binding.value//здесь мы выбираем что конкретно поменять через параметр, переданный в директиве
+      },
+      unmponted(){
+
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./theme.css"
 
-createApp(App).mount('#app')
+createApp(App)
+//добавление глобальной директивы:
+/*
+.directive("focus",{
+    
+})              (в имени префикс "v-" писать не надо)
+ */
+// .directive('focus', {
+//     mounted(el){//здесь el это элемент на который повешена директива
+//         console.log(el);//выведет на экран элемент на который повешена директива
+//         el.focus();//теперь элемент в фокусе по умолчанию
+//     }
+// })
+.mount('#app')
